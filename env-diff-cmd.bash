88e52de82a7970a9f481b4cde59e0d8afb7e7be7
Philippe Carphin <philippe.carphin@polymtl.ca>
2024-01-20 22:11:46 -0500

    First commit

---
+++ env-diff-cmd.bash
@@ -0,0 +1,216 @@
+#!/bin/bash
+
+_env_diff_root="$(cd -P $(dirname ${BASH_SOURCE[0]}) && pwd)"
+
+# In case the command is `env-diff 'PATH=""'`
+_env_diff_python_exec=$(which python3)
+_env_diff_sort=$(which sort)
+_env_diff_comm=$(which comm)
+_env_diff_jq=$(which jq)
+_env_diff_cut=$(which cut)
+_env_diff_cat=$(which cat)
+
+
+################################################################################
+# Toplevel command.  This function is separate from _env_diff_internal simply
+# to allow early returns in _env_diff_internal while still ensuring that the
+# temporary directory gets deleted.
+################################################################################
+env-diff(){
+    local _env_diff_tmpdir=$(mktemp -d tmp.XXXXXX)
+
+    local -a _env_diff_compare_args=()
+    local _env_diff_keep_tmpdir=false
+    while [[ "$1" == -* ]] ; do
+        case "$1" in
+            --list-diff) _env_diff_compare_args+=(--list-diff); shift ;;
+            --keep-tmpdir) _env_diff_keep_tmpdir=true ; shift ;;
+            *) echo "env-diff: ERROR unknown argument '$1'" >&2 ; return 1 ;;
+        esac
+    done
+
+    _env-diff-internal "$@"
+    if ! ${_env_diff_keep_tmpdir} ; then
+        rm -rf ${_env_diff_tmpdir}
+    fi
+}
+
+################################################################################
+# Main function of env-diff: We save everything, run the command, save everything
+# again and finally run the python comparison script
+################################################################################
+_env-diff-internal(){
+
+    if ! mkdir -p ${_env_diff_tmpdir}/before/functions ; then
+        return 1
+    fi
+
+    if ! mkdir -p ${_env_diff_tmpdir}/after/functions ; then
+        return 1
+    fi
+
+    if ! _env-diff-save_all_info ${_env_diff_tmpdir}/before ; then
+        echo "ERROR saving initial info" >&2
+        return 1
+    fi
+
+    if ! (
+
+        if ! eval "$@" ; then
+            echo "Command '$*' failed" >&2
+            return 1
+        fi
+
+        if ! _env-diff-save_all_info ${_env_diff_tmpdir}/after ; then
+            echo "ERROR saving final info" >&2
+            return 1
+        fi
+
+    ) ; then return 1 ; fi
+
+    if ! ${_env_diff_python_exec} ${_env_diff_root}/env-diff-compare.py "${_env_diff_compare_args[@]}" "${_env_diff_tmpdir}/before" "${_env_diff_tmpdir}/after" ; then
+        echo "ERROR in python comparison script" >&2
+        return 1
+    fi
+}
+
+################################################################################
+# Saving all info to files inside $1.
+################################################################################
+_env-diff-save_all_info(){
+    compgen -v | ${_env_diff_sort} >$1/all_vars.txt || return 1
+    compgen -e | ${_env_diff_sort} >$1/env_vars.txt || return 1
+    compgen -A arrayvar | ${_env_diff_sort} >$1/arrays.txt || return 1# includes associative arrays
+    declare -A | ${_env_diff_cut} -d ' ' -f 3 | ${_env_diff_cut} -d = -f 1 | ${_env_diff_sort} > $1/assoc_arrays.txt || return 1
+
+    # Shell variables = all_vars - env_vars - array_vars
+    ${_env_diff_comm} -23 $1/all_vars.txt $1/env_vars.txt > $1/no_env.txt || return 1
+    ${_env_diff_comm} -23 $1/no_env.txt $1/arrays.txt > $1/shell_vars.txt || return 1
+
+    # Normal arrays = array_vars - assoc_arrays
+    ${_env_diff_comm} -23 $1/arrays.txt $1/assoc_arrays.txt > $1/normal_arrays.txt || return 1
+
+    # Dump all variables to JSON
+    ${_env_diff_python_exec} -c "import os,json ; print(json.dumps(dict(os.environ)))" >$1/env_vars.json || return 1
+    <$1/shell_vars.txt _env-diff-shell_vars_to_json > $1/shell_vars.json || return 1
+    <$1/assoc_arrays.txt _env-diff_assoc_arrays_to_json > $1/assoc_arrays.json || return 1
+    <$1/normal_arrays.txt _env-diff_normal_arrays_to_json > $1/normal_arrays.json || return 1
+
+    # Save all functions to individual files
+    declare -F | ${_env_diff_cut} -d ' ' -f 3 | ${_env_diff_sort} > $1/func_names.txt || return 1
+    while read f ; do
+        type ${f} > $1/functions/BASH_FUNC_${f}.bash || return 1
+    done < $1/func_names.txt
+
+    # Save shell options
+    shopt > $1/shopt.txt || return 1
+    shopt -o > $1/shopt_set.txt || return 1
+}
+
+################################################################################
+# Save all shell variables as JSON.  Code for JQ was found in this answer on
+# stack overflow https://stackoverflow.com/a/44792751/5795941
+#
+# NOTE: Mine is different from the answer in that I use '(length-1)/2' instead
+# of 'length/2'.  For me the trailing \0 after the last element puts an empty
+# string at the end of the input stream: With length/2 the BASH array
+# ([a]=b # [c]=d) leads to an output of a\0b\0c\0d\0 which leads to the array
+# [a, b, c, d, ''] which leads to the JSON : {"a":"b", "c":"d", "":null}.
+# Each of the following functions uses JQ uses almost the same code
+#
+# In this function we use _env_diff_shell_var_name as the iteration variable
+# to avoid collisions with other variables.  This is not necessary in the
+# other _env-diff_*_to_json functions because they are not saving shell
+# variables.  The use of a name like i or _i would not be good because one
+# use for this tool is to detect variables that should be made local and doing
+# for((i=0;i<8;i++)) is the most frequent way I inadvertantly create a global
+# variable.
+################################################################################
+_env-diff-shell_vars_to_json(){
+    local _env_diff_shell_var
+    while read _env_diff_shell_var ; do
+        printf "%s\0%s\0" "${_env_diff_shell_var}" "${!_env_diff_shell_var}"
+    done | ${_env_diff_jq} -Rs 'split("\u0000")
+                    | . as $a
+                    | reduce range(0;(length-1)/2) as $i
+                     ({}; . + {($a[2*$i]): ($a[2*$i + 1])})'
+}
+
+################################################################################
+# Dump all associative arrays to a single JSON object.
+# Keys are array identifiers and values are the associative arrays themseles
+# converted to JSON.
+################################################################################
+_env-diff_assoc_arrays_to_json(){
+    printf "{\n"
+    local first=true
+    local name
+    while read name ; do
+        if [[ "${name}" == "" ]] ; then
+            continue
+        fi
+        local -n ref=${name}
+        # Ensure no comma after the last element by putting one before
+        # each element except the first one
+        if ! ${first} ; then
+            printf ","
+        fi
+        printf "\n    \"%s\": " "${name}"
+        for k in "${!ref[@]}" ; do
+            printf "%s\0%s\0" "${k}" "${ref[$k]}"
+        done | ${_env_diff_jq} -Rs 'split("\u0000")
+                        | . as $a
+                        | reduce range(0;(length-1)/2) as $i
+                        ({}; . + {($a[2*$i]): ($a[2*$i + 1])})'
+        first=false
+    done
+    printf "}"
+}
+
+################################################################################
+# Dump all normal arrays to JSON:
+# Keys are array identifiers and values are the arrays themselves as JSON arrays
+################################################################################
+_env-diff_normal_arrays_to_json(){
+    printf "{\n"
+    local first=true
+    local name
+    while read name ; do
+        local -n ref=${name}
+        if ! ${first} ; then
+            printf ","
+        fi
+        printf "\n    \"%s\": " "${name}"
+        for v in "${ref[@]}" ; do
+            printf "%s\0" "${v}"
+        done | ${_env_diff_jq} -Rs 'split("\u0000")
+                       | . as $a
+                       | reduce range(0;length-1) as $i
+                         ([] ; . + ([$a[$i]]))'
+        first=false
+    done
+    printf "}"
+}
+
+################################################################################
+# Run a simple test to manually confirm that most parts are working
+################################################################################
+_env-diff-test(){
+    g(){
+        echo "This is G"
+    }
+    echo "------------------ TEST 1"
+    env-diff 'A=B;
+    export X=Y;
+    unset USER;
+    f(){ echo "hello" ; };
+    g(){ echo "This is new G" ; };
+    declare -A assoc; assoc[y]=v;
+    BASH_ALIASES[booggers]=balls;
+    PATH=BANANNA:${PATH}:APPLE:;
+    shopt -so errexit;
+    shopt -u sourcepath;'
+    echo "------------------ TEST 2"
+    env-diff 'PATH=${PATH}:'
+}
+if [[ "$1" != "" ]] ; then _env-diff-test ; fi
---
+++ env-diff-compare.py
@@ -0,0 +1,322 @@
+import os
+import json
+import sys
+import difflib
+import re
+import argparse
+
+DESCRIPTION = """
+    Report the difference caused on the shell environment by a command.
+
+    The shell function env-diff() is the driver for this.  It saves everything
+    about the environment before and after the command in JSON files in a
+    temporary directory and calls this program to load those files and print
+    a report.
+
+    Custom comparison and display functions are used for some environment
+    variables.  Variables that are known to be colon-delimited lists are
+    compared as python sets to display elements that have been added or
+    removed.  This has the drawback that if we modify PATH by adding something
+    that was already there, it won't show in the report even if PATH was
+    modified. 
+"""
+
+comparison_functions = {}
+display_functions = {}
+ignored_variables = set(['BASHPID', 'BASH_SUBSHELL', 'EPOCHREALTIME',
+                         'EPOCHSECONDS', 'RANDOM', 'SRANDOM', 'SECONDS'])
+ignored_normal_arrays = set(['BASH_LINENO'])
+
+p = argparse.ArgumentParser()
+p.add_argument("--list-diff", action='store_true')
+p.add_argument("files", nargs=2)
+args = p.parse_args()
+
+def main():
+    """
+    Perform the entire set of comparisons
+    """
+    setup_function_dictionnaries(display_functions, comparison_functions)
+    before = ShellEnvironmentData(args.files[0])
+    after = ShellEnvironmentData(args.files[1])
+    compare_variables(before.env_vars, after.env_vars, env=True)
+    compare_variables(before.shell_vars, after.shell_vars, env=False)
+    compare_associative_arrays(before.assoc_arrays, after.assoc_arrays)
+    compare_normal_arrays(before.normal_arrays, after.normal_arrays)
+    compare_shell_options(before.shopt, after.shopt, from_set=False)
+    compare_shell_options(before.shopt_set, after.shopt_set, from_set=True)
+    compare_shell_functions(before.functions, after.functions)
+
+class ShellEnvironmentData:
+    """
+    Class ShellEnvironmentData holds the all the data saved into the temp files
+    """
+    def __init__(self, data_dir):
+        with open(os.path.join(data_dir, f"env_vars.json")) as f:
+            self.env_vars = json.load(f)
+        with open(os.path.join(data_dir, f"shell_vars.json")) as f:
+            self.shell_vars = json.load(f)
+        with open(os.path.join(data_dir, f"assoc_arrays.json")) as f:
+            self.assoc_arrays = json.load(f)
+        with open(os.path.join(data_dir, f"normal_arrays.json")) as f:
+            self.normal_arrays = json.load(f)
+        self.shopt = {}
+        with open(os.path.join(data_dir, f"shopt.txt")) as f:
+            for line in f:
+                opt, val = line.split()
+                self.shopt[opt] = val
+        self.functions = {}
+        self.shopt_set = {}
+        with open(os.path.join(data_dir, f"shopt_set.txt")) as f:
+            for line in f:
+                opt, val = line.split()
+                self.shopt_set[opt] = val
+        with open(os.path.join(data_dir, f"func_names.txt")) as f:
+            for name in f.read().splitlines():
+                with open(os.path.join(data_dir, f"functions", f"BASH_FUNC_{name}.bash")) as func:
+                    lines = func.read().splitlines()[2:]
+                    self.functions[name] = lines
+
+def compare_variables(i: dict,f: dict, env):
+    """
+    Compare sets of shell or environment variables.
+    """
+    new = set(f.keys()) - set(i.keys())
+    deleted = set(i.keys()) - set(f.keys())
+    common = set(i.keys()).intersection(set(f.keys())) - set(ignored_variables)
+    changed = list(sorted(filter(lambda v: i[v] != f[v], common)))
+
+    if new or deleted or changed:
+        if env:
+            print("\033[1m================= ENVIRONMENT VARIABLES ================\033[0m")
+        else:
+            print("\033[1m================= SHELL VARIABLES ================\033[0m")
+
+    if new:
+        print('\033[4;32mNew variables\033[0m')
+        for var in sorted(new):
+            display_single_variable(var, f[var])
+
+    if deleted:
+        print('\033[4;31mDeleted variables\033[0m')
+        for var in sorted(deleted):
+            display_single_variable(var, i[var])
+
+    if changed:
+        print('\033[4;33mModified variables\033[0m')
+        for var in changed:
+            for k in comparison_functions:
+                if re.fullmatch(k, var):
+                    compare_func = comparison_functions[k]
+                    break
+            else:
+                compare_func = lambda n,i,f: print(f"{n}:\n\tOLD: {i}\n\tNEW: {f}")
+            compare_func(var, i[var], f[var])
+
+def compare_associative_arrays(i: dict,f: dict):
+    """
+    Print differences between the sets of associative arrays before and after
+    """
+    new = set(f.keys()) - set(i.keys())
+    deleted = set(i.keys()) - set(f.keys())
+    common = set(i.keys()).intersection(set(f.keys()))
+    changed = list(sorted(filter(lambda v: i[v] != f[v], common)))
+
+    if new or deleted or changed:
+        print("\033[1m================= ASSOCIATIVE ARRAY VARIABLES ================\033[0m")
+
+    if new:
+        print('\033[4;32mNew Associative Arrays\033[0m')
+        for var in sorted(new):
+            print(f"{var}: {f[var]}")
+
+    if deleted:
+        print('\033[4;31mDeleted Associative Arrays\033[0m')
+        for var in sorted(deleted):
+            print(f"{var}: {f[var]}")
+
+    if changed:
+        print('\033[4;33mModified Associative Arrays\033[0m')
+        for var in sorted(filter(lambda v: i[v] != f[v], common)):
+            print(f"Initial {var}: {i[var]}")
+            print(f"Final   {var}: {f[var]}")
+
+def compare_normal_arrays(i: dict, f:dict):
+    """
+    Print differences between the sets of normal arrays before
+    This is the exact same code as the one for associative arrays
+    except for the words "Normal" and "Associative" in the prints.
+    I'm leaving it like because I intend to display new arrays
+    and compare modified ones in in different ways in the future.
+    """
+    new = set(f.keys()) - set(i.keys())
+    deleted = set(i.keys()) - set(f.keys())
+    common = set(i.keys()).intersection(set(f.keys())) - ignored_normal_arrays
+    changed = sorted(filter(lambda v: i[v] != f[v], common))
+
+    if new or deleted or changed:
+        print("\033[1m================= NORMAL ARRAY VARIABLES ================\033[0m")
+
+    if new:
+        print('\033[4;32mNew Normal Arrays\033[0m')
+        for var in sorted(new):
+            print(f"{var}: {f[var]}")
+
+    if deleted:
+        print('\033[4;31mDeleted Normal Arrays\033[0m')
+        for var in sorted(deleted):
+            print(f"{var}: {f[var]}")
+
+    if changed:
+        print('\033[4;33mModified Normal Arrays\033[0m')
+        for var in changed:
+            print(f"Initial {var}: {i[var]}")
+            print(f"Final   {var}: {f[var]}")
+
+def compare_shell_options(i: dict, f: dict, from_set=False):
+    """
+    Print differences between shell options
+    """
+    if set(i.keys()) != set(f.keys()):
+        print(set(i.keys()).difference(set(f.keys())))
+    changes = []
+    for k in i:
+        if i[k] != f[k]:
+            changes.append(f"{k}: {i[k]} -> {f[k]}")
+    if changes:
+        if from_set:
+            print("\033[1m================= SHELL OPTIONS (SET) ================\033[0m")
+        else:
+            print("\033[1m================= SHELL OPTIONS ================\033[0m")
+        print('\n'.join(changes))
+
+
+def compare_shell_functions(i: dict, f: dict, show_new_defs=True):
+    """
+    Compare sets of shell functions
+    """
+    new = set(f.keys()) - set(i.keys())
+    deleted = set(i.keys()) - set(f.keys())
+    common = set(i.keys()).intersection(set(f.keys()))
+    changed = sorted(filter(lambda v: i[v] != f[v], common))
+
+    if new or deleted or changed:
+        print("\033[1m================= SHELL FUNCTIONS ================\033[0m")
+    
+    if new:
+        print('\033[4;32mNew functions\033[0m')
+        for func in sorted(new):
+            if show_new_defs:
+                print(f"{func}()", end='')
+                print('\n'.join(f[func]))
+            else:
+                print(func)
+
+    if deleted:
+        print('\033[4;31mDeleted functions\033[0m')
+        for func in sorted(deleted):
+            print(f"{func}()\n")
+
+    if changed:
+        print('\033[4;33mModified functions\033[0m')
+        for func in changed:
+            print(f"\033[1;35m{func}()\033[0m")
+            compare_bash_func(i[func], f[func])
+
+################################################################################
+# Display and comparison functions for individual variables, arrays and functions
+################################################################################
+def display_single_variable(name, value):
+    for k in display_functions:
+        if re.fullmatch(k, name):
+            display_func = display_functions[k]
+    else:
+        display_func = lambda n,v: print(f"{n}={v}")
+    display_func(name, value)
+
+def compare_colon_lists(name, initial_value, final_value):
+    initial_list = initial_value.split(':')
+    final_list = final_value.split(':')
+    return compare_python_lists(name, initial_list, final_list)
+
+def compare_space_lists(name, initial_value, final_value):
+    initial_list = initial_value.split(' ')
+    final_list = final_value.split(' ')
+    return compare_python_lists(name, initial_list, final_list)
+
+def color_full_diff(before, after):
+    diff_colors = {'+': '\033[32m', ' ': '', '-': '\033[31m', '?': '\033[36m'}
+    for l in difflib.unified_diff(before, after, fromfile="before", tofile="after", n=1000):
+        if l.startswith('+++') or l.startswith('---') or l.startswith('@@'):
+            continue
+        color = diff_colors.get(l[0],'')
+        yield f"{color}{l.rstrip()}\033[0m"
+
+def compare_python_lists(name, initial_list, final_list, show_kept=False):
+    initial_set = set(initial_list)
+    final_set = set(final_list)
+    new = set(final_list) - set(initial_list)
+    deleted = set(initial_list) - set(final_list)
+    common = set(initial_list).intersection(set(final_list))
+    indent = '        '
+    if args.list_diff:
+        print(name)
+        print('    ' + '\n    '.join(map(str.rstrip, list(color_full_diff(
+                [ s if s else "(empty)" for s in initial_list ],
+                [ s if s else "(empty)" for s in final_list ]
+        )))))
+    else:
+        print(f"{name} (colon-separated list using set comparison)")
+        if new:
+            print('    ADDED:')
+            print('\n'.join([f'{indent}{e}' for e in new]))
+        if common and show_kept:
+            print('    KEPT:')
+            print('\n'.join([f'{indent}{e}' for e in common]))
+        if deleted:
+            print('    REMOVED:')
+            print('\n'.join([f'{indent}{e}' for e in deleted]))
+        if initial_list != final_list and (initial_set == final_set):
+            print(f'    (Before and after are different as lists but not as sets)')
+            print(f'    (It could be that adding an element that was already there,)')
+            print(f'    (or removing doubles, reordering, or adding separators)')
+
+def setup_function_dictionnaries(display, comparison):
+    comparison_functions['BASH_FUNC_[a-zA-Z_.-]*%%'] = compare_exported_bash_func
+    colon_lists = [
+        'SSMUSE_LOADED', 'CDPATH', 'PATH', 'LD_LIBRARY_PATH', 'DYLD_LIBRARY_PATH',
+        'CPATH', 'MIC_LD_LIBRARY_PATH', 'INFOPATH', 'OBJC_INCLUDE_PATH', 'NLSPATH',
+        'LIBRARY_PATH', 'SSM_INCLUDE_PATH', 'CPLUS_INCLUDE_PATH', 'C_INCLUDE_PATH',
+        'MANPATH', 'EC_INCLUDE_PATH', 'LIBPATH', 'PYTHONPATH', 'TCL_LIBRARY',
+        'LD_LIB_PATH', 'CRAY_LD_LIBRARY_PATH', 'LOADEDMODULES', '_LMFILES_',
+        'PKG_CONFIG_PATH', 'ACLOCAL_PATH', 'CMAKE_PREFIX_PATH',
+        'EC_CMAKE_MODULE_PATH', 'MODULEPATH', 'BASHOPTS', 'SHELLOPTS'
+    ]
+    space_lists = [
+        'EC_LD_LIBRARY_PATH', 'SSMUSE_PLATFORMS', 'JOBCTL_PBS_CELLS',
+        'ORDENV_PLATFORMS', 'ORDENV_USER_PROFILES',
+        'ORDENV_USER_PROFILE_FILENAMES'
+    ]
+    for n in colon_lists:
+        comparison[n] = compare_colon_lists
+    for n in space_lists:
+        comparison[n] = compare_space_lists
+
+def compare_exported_bash_func(name, before, after):
+    print(name)
+    compare_bash_func(before.splitlines(), after.splitlines())
+
+def compare_bash_func(before, after):
+    diff_colors = {'+': '\033[32m', ' ': '', '-': '\033[31m', '?': '\033[36m'}
+    def diff():
+        for l in difflib.unified_diff(before, after, fromfile="before", tofile="after"):
+            if l.startswith('+++') or l.startswith('---') or l.startswith('@@'):
+                continue
+            color = '\033[1;34m' \
+                    if l.startswith('+++') or l.startswith('---') or l.startswith('@@') \
+                    else diff_colors.get(l[0],'')
+            yield f"{color}{l.strip()}\033[0m"
+    print('\n'.join(diff()))
+
+if __name__ == "__main__":
+    main()
