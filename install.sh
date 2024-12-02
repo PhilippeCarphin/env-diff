1e0915618c6f1c99ed987aacdb5076b9e073e402
Philippe Carphin <phil103@hotmail.com>
2024-12-02 00:08:24 -0500

    Use env for shebang for MacOS

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 this_dir=$(cd -P $(dirname $0) && pwd)
 echo "Add \`source $this_dir/env-diff-cmd.bash\` in your ~/.bashrc"
 echo "You can also \`cp dot-config-env-diff-cmd.yml $HOME/.config/env-diff.yml\`"
